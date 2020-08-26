4ad58f8c9823ed8be244150ed9a84a8e74bc42b1
Akeem Bello <akeembello10@gmail.com>
2020-08-26 19:59:34 +0100

    bootsrap registration functionality

--- Register.php
+++ Register.php
@@ -1,3 +1,15 @@
+<?php # handle registration form
+
+#be sure user clicked the submit button
+
+if(array_key_exists('register', $_POST)) {
+    var_dump($_POST);
+
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,30 +20,33 @@
         <h1>Register</h1>
         <hr />
 
-        <form>
+        
+        <form method="POST" action="">
             <div>
                <label>First Name</label><br />
-               <input type="text" />
+               <input type="text" name="fn"/>
 
            </div>
 
             <div>
                  <label>Last Name</label><br />
-                 <input type="text" />
+                 <input type="text" name="ln"/>
             
             </div>
 
             <div>
                 <label>Email</label><br />
-                <input type="email" />
+                <input type="email" name="e"/>
 
             </div>
 
             <div>
 
                  <label>Password</label><br />
-                  <input type="password" />
-            </div> 
+                  <input type="password" name="p"/>
+            </div>
+
+            <div><input type="submit" name="register" value="Register"/></div>
         </form>     
     </body>
 </html>
