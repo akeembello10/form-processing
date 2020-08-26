1527fa917202f968a49b29183e2de3540578a5ec
Akeem Bello <akeembello10@gmail.com>
2020-08-26 19:21:34 +0100

    create registration form

---
+++ Register.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+
+    <body>
+        <h1>Register</h1>
+        <hr />
+
+        <form>
+            <div>
+               <label>First Name</label><br />
+               <input type="text" />
+
+           </div>
+
+            <div>
+                 <label>Last Name</label><br />
+                 <input type="text" />
+            
+            </div>
+
+            <div>
+                <label>Email</label><br />
+                <input type="email" />
+
+            </div>
+
+            <div>
+
+                 <label>Password</label><br />
+                  <input type="password" />
+            </div> 
+        </form>     
+    </body>
+</html>
